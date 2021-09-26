c3f4250e4664321a74e96c76011e53721b25c83d
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2021-09-27 00:51:37 +0300

    Code was organized and some variables name was changed

--- blocks.h
+++ blocks.h
@@ -2,10 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "date '+%b %d (%a) %I:%M:%S%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//[] -> tell the compiler to determine the size of the array which equals 4 now
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = sizeof(delim);
